e1cc8245cbe808b968e8960f9fb891c0c9a748f0
Akhand Agrawal <83289867+AkhandAgrawal@users.noreply.github.com>
2024-03-29 10:47:27 +0530

    Update inference_classifier.py

--- inference_classifier.py
+++ inference_classifier.py
@@ -32,9 +32,9 @@ while True:
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
             mp_drawing.draw_landmarks(
-                frame,  # image to draw
-                hand_landmarks,  # model output
-                mp_hands.HAND_CONNECTIONS,  # hand connections
+                frame, 
+                hand_landmarks,  
+                mp_hands.HAND_CONNECTIONS,  
                 mp_drawing_styles.get_default_hand_landmarks_style(),
                 mp_drawing_styles.get_default_hand_connections_style())
 
